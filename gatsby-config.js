181da775925afe44279bdf41cf9a2ec63a01f06e
lm9 <depkey@me.com>
2018-07-02 10:33:10 +0900

    tabWidth:4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  siteMetadata: {
-    title: 'れもくすサイト',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript',
-    'gatsby-transformer-remark',
-     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src`,
-      },
-    },
-  ],
+	siteMetadata: {
+		title: 'れもくすサイト',
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-typescript',
+		'gatsby-transformer-remark',
+		 {
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `src`,
+				path: `${__dirname}/src`,
+			},
+		},
+	],
 }
