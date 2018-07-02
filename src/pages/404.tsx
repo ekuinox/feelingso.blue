d94a14b46502e65dd7f2fdd8e75089b2c0b36e48
lm9 <depkey@me.com>
2018-07-02 14:04:37 +0900

    くるくる終わった終わった

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
+import './404.css'
 
 export default class NotFoundPage extends React.Component {
+	private _w = 100
+	private _h = 100
 	render() {
 		return (
 			<div>
 				<h1>NOT FOUND</h1>
 				<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-				<img src={'/assets/img/IMG_0202.JPG'} />
+				<img id={'not-found-img'} src={'/assets/img/IMG_0202.JPG'} />
 			</div>
 		)
 	}
