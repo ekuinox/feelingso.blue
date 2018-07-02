80dea52b009786271a028d2d0fc02ad9dd2ad7e9
lm9 <depkey@me.com>
2018-07-02 12:13:28 +0900

    staticこんにちは

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,6 +6,7 @@ export default class NotFoundPage extends React.Component {
 			<div>
 				<h1>NOT FOUND</h1>
 				<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+				<img src={'/assets/img/IMG_0202.JPG'} />
 			</div>
 		)
 	}
