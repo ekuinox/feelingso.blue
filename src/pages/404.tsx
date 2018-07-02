c74e59c3d7a45918e9313b3555230e1b578260a4
lm9 <depkey@me.com>
2018-07-02 14:30:51 +0900

    title

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react'
+import Helmet from 'react-helmet'
 import './404.css'
 
-export default class NotFoundPage extends React.Component {
+interface Props {
+	data: {
+		site: {
+			siteMetadata: {
+				title: string
+			}
+		}
+	}
+}
+
+export default class NotFoundPage extends React.Component<Props> {
+	readonly pageTitle = "404"
+	constructor(props: Props) {
+		super(props)
+	}
 	render() {
 		return (
 			<div>
+				<Helmet title={`${this.pageTitle} - ${this.props.data.site.siteMetadata.title}`} />
 				<h1>NOT FOUND</h1>
 				<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
 				<img id={'not-found-img'} src={'/assets/img/IMG_0202.JPG'} />
@@ -12,3 +28,14 @@ export default class NotFoundPage extends React.Component {
 		)
 	}
 }
+
+declare function graphql(x: TemplateStringsArray): any
+export const query = graphql`
+	query NotFoundPageQuery {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+	}
+`
